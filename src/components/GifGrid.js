a03fb3f32c9099ec7db00836174632b6a87825c1
CS-WKS-1 <Rec-Zen@iMac-de-CS-WKS-1.local>
2021-11-25 10:18:22 -0600

    use fetch gifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,26 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-// import { GifGridItem } from './GifGridItem';
-// import {getGifs} from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
+
 
 
 export const GifGrid = ({category}) => {
 
-    // const [images, setImages] = useState([])
-   const {data, loading} = useFetchGifs();
     
-    // useEffect(()=>{
-    //     getGifs(category)
-    //     .then(setImages)
-    // }, [category]);
+   const {data:images, loading} = useFetchGifs(category);
+    
+   
 
     return (
         <>
         <h3>{category}</h3>
-        {loading ? 'Cargando...':'data cargada'}
-        {/* <div className="card-grid">
+            {/* {loading ? <p>Loading</p> : null} si es true muestra loading, caso contrario no muestra nada */}
+            {loading && <p>Loading</p>}// si es true muestra el loading , caso contrario no muestra nada
+         <div className="card-grid">
          
-           
                 {
                     images.map( img => (
                         <GifGridItem 
@@ -30,10 +27,7 @@ export const GifGrid = ({category}) => {
                         />
                     ))
                 }
-          
-            
-          
-        </div> */}
+        </div> 
         </>
     )
 }
